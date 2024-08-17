2ebc5a60327a8a866b7d3cda50d602c47f9a9302
Maha-Muthu <mahalaxmi.muthuvel@gmail.com>
2024-08-17 10:52:02 -0400

    Updated Ingestion FIles and Schema

--- ingestion_script_files/Movie_Review_Ingestion.sql
+++ ingestion_script_files/Movie_Review_Ingestion.sql
@@ -14,35 +14,38 @@ CREATE TABLE movie_review.Movie_Rating (
     movie_id INTEGER,
     user_id INTEGER,
     rating INTEGER,
-    created_on DATE,
     PRIMARY KEY (movie_id,user_id)
 );
 
 
 INSERT INTO movie_review.Movies VALUES (1, 'Inception'),
 (2, 'The Matrix'),
-(3, 'Interstellar'),
+(7, 'Interstellar'),
 (4, 'The Dark Knight'),
-(5, 'Pulp Fiction');
+(5, 'Pulp Fiction'),
+(6, 'Gladiator'),
+(3, 'Avatar');
 
 INSERT INTO movie_review.Users  VALUES  (1, 'Alice'),
 (2, 'Bob'),
 (3, 'Charlie'),
 (4, 'Diana'),
-(5, 'Eve');
+(6, 'Eve'),
+(5, 'James'),
+(7, 'Isabella');
 
-INSERT INTO movie_review.Movie_rating VALUES (1, 1, 9, '2023-01-01'),
-(2, 1, 8, '2023-01-05'),
-(3, 2, 10, '2023-02-10'),
-(4, 2, 9, '2023-02-15'),
-(5, 3, 7, '2023-03-01'),
-(1, 3, 9, '2023-03-05'),
-(2, 4, 8, '2023-04-01'),
-(3, 4, 10, '2023-04-05'),
-(4, 5, 8, '2023-05-10'),
-(5, 5, 9, '2023-05-15'),
-(1, 4, 9, '2023-06-01'),
-(3, 1, 10, '2023-06-10'),
-(4, 3, 9, '2023-07-01'),
-(2, 5, 7, '2023-07-05'),
-(5, 2, 8, '2023-08-01');
+INSERT INTO movie_review.Movie_rating VALUES (1, 1, 9),
+(2, 1, 8),
+(7, 2, 10),
+(4, 2, 9),
+(5, 3, 7),
+(1, 3, 9),
+(2, 4, 8),
+(7, 4, 10),
+(4, 6, 8),
+(5, 6, 9),
+(1, 4, 9),
+(7, 1, 10),
+(4, 3, 9),
+(2, 6, 7),
+(5, 2, 8);
